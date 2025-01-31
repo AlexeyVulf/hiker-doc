9dd51f2de4cc256660272665045e659ecb64ec79
User <user@Users-MacBook-Pro.local>
2025-01-31 15:13:58 +0300

    fix_desc_2

--- docs/hikerapi/__version__.py
+++ docs/hikerapi/__version__.py
@@ -1,4 +1,4 @@
 __title__ = "hikerapi"
-__description__ = "hikerapi client, for Python 3."
+__description__ = "HikerAPI client, for Python 3."
 __version__ = "1.6.3"
 __domain__ = "api.hikerapi.com"
